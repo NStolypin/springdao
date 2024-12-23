359a2ed76bc62eb86bf7936ca8dfdb01144927ab
NStolypin <i@nstolypin.ru>
2024-12-23 18:18:03 +0300

    final simple CRUD

--- src/main/java/ru/esplit/config/MySpringMVCDispatcherServletInitializer.java
+++ src/main/java/ru/esplit/config/MySpringMVCDispatcherServletInitializer.java
@@ -1,10 +1,14 @@
 package ru.esplit.config;
 
 import org.springframework.lang.NonNull;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
 public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
@@ -12,12 +16,23 @@ public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationC
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {SpringConfig.class};
+        return new Class[] { SpringConfig.class };
     }
 
     @Override
     @NonNull
     protected String[] getServletMappings() {
-        return new String[] {"/"};
+        return new String[] { "/" };
+    }
+
+    @Override
+    public void onStartup(@NonNull ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 }
